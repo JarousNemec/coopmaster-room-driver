bb4150b219da1f317c9a4b67df537488e2ba0d7c
jarin.nemec <jarin.nemec@gmail.com>
2025-01-02 18:11:10 +0100

    updated arduino_reader.py

--- app/blueprints/room_blueprint.py
+++ app/blueprints/room_blueprint.py
@@ -1,4 +1,6 @@
 import json
+import logging
+
 from flask import Blueprint, make_response
 
 from app.configuration import arduino
@@ -8,27 +10,35 @@ room_blueprint = Blueprint('room_blueprint', __name__)
 
 @room_blueprint.route("/api/temperature", methods=['GET'])
 def get_temperature_endpoint():
-    value = json.loads(arduino.run_command('j'))["temperature"]
+    arduino_response = arduino.run_command('j')
+    logging.info("get get_temperature_endpoint: " + arduino_response)
+    value = json.loads(arduino_response)["temperature"]
     response = {'value': value, 'unit': "C"}
     return make_response(response)
 
 
 @room_blueprint.route("/api/humidity", methods=['GET'])
 def get_humidity_endpoint():
-    value = json.loads(arduino.run_command('j'))["humidity"]
+    arduino_response = arduino.run_command('j')
+    logging.info("get get_humidity_endpoint: " + arduino_response)
+    value = json.loads(arduino_response)["humidity"]
     response = {'value': value, 'unit': '%'}
     return make_response(response)
 
 
 @room_blueprint.route("/api/lamp/state", methods=['GET'])
 def get_lamp_endpoint():
-    response = json.loads(arduino.run_command('s'))["lamp"]
+    arduino_response = arduino.run_command('s')
+    logging.info("get get_lamp_endpoint: " + arduino_response)
+    response = json.loads(arduino_response)["lamp"]
     return make_response(response)
 
 
 @room_blueprint.route("/api/door/state", methods=['GET'])
 def get_door_endpoint():
-    response = json.loads(arduino.run_command('s'))["door"]
+    arduino_response = arduino.run_command('s')
+    logging.info("get get_door_endpoint: " + arduino_response)
+    response = json.loads(arduino_response)["door"]
     return make_response(response)
 
 
